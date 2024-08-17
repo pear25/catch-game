f8ca8a4466a35cb81a5db8b5b4a1873e419a924b
Pierson Limas <piersonlimas12@gmail.com>
2024-08-17 14:31:24 +0700

    add fonts and images to public

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'main-menu': "url('./public/images/bg1.png')",
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        jersey: ['Jersey 10', 'serif'],
+      },
+    },
   },
   plugins: [],
 };
