3a404c72317615048b898d2baf56b89c325cd246
Pierson Lim <piersonlimas12@gmail.com>
2024-08-23 09:58:26 +0800

    fix: add line break for long names, adjust lb size

--- src/components/LeaderboardScoreEntry.tsx
+++ src/components/LeaderboardScoreEntry.tsx
@@ -12,7 +12,9 @@ export const LeaderboardScoreEntry = ({
   return (
     <div className="flex gap-12 sm:gap-32 justify-between items-center w py-2 z-0 px-10 md:text-md text-sm">
       <p>{rank}</p>
-      <p>{name}</p>
+      <p className="break-words max-w-[120px] sm:max-w-[200px] md:max-w-[350px] lg:max-w-fit">
+        {name}
+      </p>
       <p>{score}</p>
     </div>
   );
